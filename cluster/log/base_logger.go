c5d4301c5091cff2b11f35045699e2cd7b589349
YeonwooSung <neos960518@gmail.com>
2024-11-15 23:57:49 +0900

    feat: Implement the cluster logging module

---
+++ cluster/log/base_logger.go
@@ -0,0 +1,61 @@
+package log
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+var RavenLogger Logger = Logger{}
+
+type Logger struct {
+	generalLogger *log.Logger
+	errorLogger   *log.Logger
+}
+
+// Logger has method "initLoggers", which initialize loggers with basic settings
+func (l *Logger) InitLoggers() {
+	absPath, err := filepath.Abs("../log")
+	if err != nil {
+		fmt.Println("Error reading given path:", err)
+	}
+
+	generalLog, err := os.OpenFile(absPath+"/general-log.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		os.Exit(1)
+	}
+	errorLog, err := os.OpenFile(absPath+"/error-log.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		os.Exit(1)
+	}
+
+	l.generalLogger = log.New(generalLog, "General Logger:\t", log.Ldate|log.Ltime|log.Lshortfile)
+	l.errorLogger = log.New(errorLog, "Error Logger:\t", log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+func (l Logger) LogDebug(msg string) {
+	l.generalLogger.Printf("[Debug]: %s", msg)
+}
+
+func (l Logger) LogInfo(msg string) {
+	l.generalLogger.Printf("[Info]: %s", msg)
+}
+
+func (l Logger) LogWarning(msg string) {
+	l.generalLogger.Printf("[Warn]: %s", msg)
+	l.errorLogger.Printf("[Warn]: %s", msg)
+}
+
+func (l Logger) LogError(msg string) {
+	l.generalLogger.Printf("[Error]: %s", msg)
+	l.errorLogger.Printf("[Error]: %s", msg)
+}
+
+// LogCritical exits the application, since the "critical" error might affect to application critically
+func (l Logger) LogCritical(msg string) {
+	l.generalLogger.Printf("[Critical]: %s", msg)
+	l.errorLogger.Fatalf("[Critical]: %s", msg)
+}
